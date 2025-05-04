24bc1f18188f93311bf6f16dba7cbda4faa56fa0
Mohannad Najjar <mohannadnaj@me.com>
2025-05-04 09:07:32 +0300

    manifest v3

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.executeScript(activeTab.id, {"file": "content.js"});
+chrome.action.onClicked.addListener(function(tab) {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ["content.js"]
   });
 });
